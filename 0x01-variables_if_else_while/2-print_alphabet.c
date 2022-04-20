c43ab73d54295888db661bde58ed599506ad611c
Brsh77 <berhanabebe63@gmail.com>
2022-04-20 02:32:11 -0700

    connection check

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* main checks for alpha character a to z
+ * 
+ * returns 1 when printing a to z 
+ */
+int main(void)
+char (alpha=a)
+{
+for(char alpha = 'a'; alpha <=z ; alpha++)
+{
+	putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
