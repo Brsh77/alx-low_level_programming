c73f3a62214866e52c4e7daaeb0214ec7f9612e4
Brsh77 <berhanabebe63@gmail.com>
2022-04-22 08:18:48 -0700

    Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - checks for alpha character a to Z
+*
+* Return: 1 when printing a to Z
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+return (0);
+}
