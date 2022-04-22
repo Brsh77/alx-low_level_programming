2159069111323883181c84941baf1dc44df80f1a
Brsh77 <berhanabebe63@gmail.com>
2022-04-22 09:56:14 -0700

    Write a program that prints all the numbers of base 16 in lowercase, followed by a new line and  can only use putchar three times

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char hex;
 
-	for (hex = '0'; hex<= '9'; hex++)
+	for (hex = '0'; hex <= '9'; hex++)
 		putchar(hex);
-	for (hex = 'a'; hex<= 'f'; hex++)
+	for (hex = 'a'; hex <= 'f'; hex++)
 		putchar(hex);
 putchar('\n');
 return (0);
