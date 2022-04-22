1e2b191948b87fae7effa5a96ba0a298f4ad18ba
Brsh77 <berhanabebe63@gmail.com>
2022-04-22 13:33:47 -0700

    A program that prints all possible different combinations of three digits that are different and only the smallest combination of ascending digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+/**
+* main - checks for combination of triple digit number character
+*
+* Return: 1 when printing 012 to 789 with comma and space
+*/
+
+int main(void)
+{
+	int c;
+	int d;
+	int e = 0;
+
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
+
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				c++;
+			}
+			d++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
