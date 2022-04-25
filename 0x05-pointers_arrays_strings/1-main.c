6d96bcc7004534581b264f77e43fdf21bdbcd4b4
Brsh77 <berhanabebe63@gmail.com>
2022-04-25 02:58:35 -0700

    A function that swaps the values of two integers and Dont swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int a;
+    int b;
+
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    a = 42; b = 98;
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
+}
