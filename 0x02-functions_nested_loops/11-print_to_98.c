8203901fd167242501e4f6dcdc883e595206e056
Brsh77 <berhanabebe63@gmail.com>
2022-04-27 03:46:19 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself writes  a function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 /**
  * print_to_98 - print to 98
+ * Numbers must be separated by a comma, followed by a space
+ * Numbers should be printed in order
+ * The first printed number should be the number passed to your function
+ * The last printed number should be 98
+ * You are allowed to use the standard library
  * @n : number to start from
  * Return:0 or 1
  */
