14fa03454e2cf5fe003891eb026996e4d2f44619
Brsh77 <berhanabebe63@gmail.com>
2022-04-28 08:16:24 -0700

    Once an idea has taken hold of the brain its almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
+ * _pow_recursion - Search a string for any of a set of bytes.
  * discription - that returns the value of x raised to the power of y
  * If y is lower than 0, the function should return -1
- * discription - _pow_recursion - Search a string for any of a set of bytes.
  * @x: base
  * @y: exposant
  * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
