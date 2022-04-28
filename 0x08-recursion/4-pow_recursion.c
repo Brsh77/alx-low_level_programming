1e92c810cb20bb53eadaa2ced37a8d33b2869290
Brsh77 <berhanabebe63@gmail.com>
2022-04-28 07:59:24 -0700

    Once an idea has taken hold of the brain its almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * a function that returns the value of x raised to the power of y
+ * If y is lower than 0, the function should return -1
+ * _pow_recursion - Search a string for any of a set of bytes.
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+if (y < 0)
+	return (-1);
+else if (y == 1)
+	return (x);
+else if (y == 0)
+	return (1);
+
+return (x * _pow_recursion(x, y - 1));
+
+}
