21ba77c42928e1c834129c60dcb6484b34ee6fe2
Brsh77 <berhanabebe63@gmail.com>
2022-04-28 09:52:44 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * If n does not have a natural square root, the function should return -1
+ * @n: int
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+return (square(n, 1));
+}
+
+/**
+ * square - find square root
+ * @n: int to find square root
+ * @val: square root
+ * Return: int
+ */
+
+int square(int n, int val)
+{
+
+if (val * val == n)
+	return (val);
+else if (val * val < n)
+	return (square(n, val + 1));
+else
+	return (-1);
+
+}
