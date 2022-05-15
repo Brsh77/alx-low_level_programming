718c6e6d87e24f7a695c0cbf26d32966d60bffb8
Brsh77 <berhanabebe63@gmail.com>
2022-05-15 11:48:02 -0700

    9. Numbers have life they are not just symbols on paper - Write a function that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_number - print integers
+ * @n: integer value.
+ * You are not allowed to use long
+ * You can only use _putchar function to print
+ * You are not allowed to use arrays or pointers
+ * You are not allowed to hard-code special values
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int a, b, count, tmp, pow;
+
+	a = n;
+	pow = b = 1;
+
+	if (n < 0)
+	{
+		a *= -1;
+		_putchar('-');
+	}
+
+	tmp = a;
+
+	while (tmp > 9)
+	{
+		b++;
+		tmp /= 10;
+	}
+
+	for (count = 1; count < b; count++)
+		pow *= 10;
+
+	while (pow > 1)
+	{
+		_putchar((a / pow) % 10 + '0');
+		pow = pow / 10;
+	}
+	_putchar(a % 10 + '0');
+}
